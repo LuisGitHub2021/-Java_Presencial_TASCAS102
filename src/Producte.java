373a7a775da342bcdf8ad819be8856b408bfd7a3
LuisGitHub2021 <96303921+LuisGitHub2021@users.noreply.github.com>
2023-10-23 17:37:30 +0200

    inicio proyecto tasca S102

---
+++ src/Producte.java
@@ -0,0 +1,14 @@
+
+public class Producte {
+
+	private String nom;
+	private double preu;
+	
+	
+	// constructor
+	private Producte(String nom, double preu) {
+		this.nom=nom;
+		this.preu=preu;
+	}
+
+}
